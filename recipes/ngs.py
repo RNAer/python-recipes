fae314c96e4de25962e8bdf712ed6e44b7955d09
Zech Xu <zhenjiang.xu@gmail.com>
2016-06-09 17:11:58 -0700

    add compute_n50() and update create_sample_table()

--- recipes/ngs.py
+++ recipes/ngs.py
@@ -1,13 +1,62 @@
 import re
 import os
 import pandas as pd
+from collections import Iterator
 
 
-def form_sample_table(d,
-                      patterns=None,
-                      sid=lambda x: re.split('_L00[0-9]_R[12]_', x)[0],
-                      select=lambda x: x.endswith('.fq'), negate=False):
-    '''Return tables of the raw sequence files.
+def compute_n50(nums, cutoff=500):
+    '''Compute N50 of the input contig lengths.
+
+    Parameters
+    ----------
+    nums : Iterable, int
+        contig lengths
+    cutoff : int
+        only consider the contigs >= cutoff.
+    Returns
+    -------
+    int
+
+    Examples
+    --------
+    >>> from . import compute_n50
+    >>> nums = [400, 500, 502, 655, 634, 605, 590, 584, 552, 549, 545, 545, 542, 536, 526, 521, 517, 513]
+    >>> compute_n50(nums)
+    ... 545
+    '''
+    if isinstance(nums, Iterator):
+        raise TypeError('Must supply a container, not an iterator.')
+    n50_len = sum(i for i in nums if i >= cutoff) / 2
+    length = 0
+    for i in sorted(nums, reverse=True):
+        length += i
+        if length >= n50_len:
+            return i
+
+
+def create_sample_table(d, patterns=None,
+                        sid=lambda x: re.split('_L00[0-9]_R[12]_', x)[0],
+                        select=lambda x: x.endswith('.fq.gz'),
+                        negate=False):
+    '''Return a sample-by-file-path table of the raw sequence files.
+
+    Parameters
+    ----------
+    d : str
+        dir to the raw sequence files
+    patterns : dict-like
+        file patterns for each sample. keys will be used as column name
+        of the return table and values will be used in `re.search`
+    sid : callable
+        how to create sample IDs from sequence file name
+    select : callable
+        what files to include
+    negate : boolean
+        negate the select.
+
+    Returns
+    -------
+    pandas.DataFrame
     '''
     if patterns is None:
         patterns = {'R1_paired':   r'R1_[0-9]{3}_paired',
@@ -24,12 +73,12 @@ def form_sample_table(d,
             try:
                 s_id = sid(f)
             except:
-                raise ValueError('Can not extract sample ID from file name: %s'
-                                 % f)
-            # check the file name only satisfy one pattern
+                raise ValueError('Can not extract sample ID from file name: %s' % f)
+            # check the file name only matches one unique pattern
             t = [i for i in patterns if re.search(patterns[i], f)]
-            assert len(t) == 1, ('File name %s matches none or '
-                                 'multiple patterns.') % f
+            if len(t) != 1:
+                raise ValueError('File name {} matches none or multiple patterns: {}'.format(f, t))
+
             samples.loc[s_id, t[0]] = f
 
     return samples
