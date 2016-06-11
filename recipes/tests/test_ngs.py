73fcbfd59525f52dd6947872fc52128f3bb7a8ce
Zech Xu <zhenjiang.xu@gmail.com>
2016-06-11 16:49:11 -0700

    add test for create_sample_table

--- recipes/tests/test_ngs.py
+++ recipes/tests/test_ngs.py
@@ -1,12 +1,31 @@
-from __future__ import absolute_import, division, print_function
-
 import unittest
-from tempfile import NamedTemporaryFile, mkdtemp
-from os.path import exists, join
-from shutil import rmtree
+from tempfile import TemporaryDirectory
+from os.path import join
+import re
 
-class TestFormSampleTable(unittest.TestCase):
-    pass
+import numpy as np
+import pandas as pd
+
+from recipes.ngs import create_sample_table
+
+
+class Tests(unittest.TestCase):
+    def test_create_sample_table(self):
+        with TemporaryDirectory() as d:
+            sids = ['285', 'Dust.1', 'Dust.2']
+            types = ['R1_paired', 'R1_unpaired', 'R2_paired', 'R2_unpaired']
+            files = np.array(['{}_{}.fq'.format(i, j) for i in sids for j in types])
+            exp = pd.DataFrame(data=files.reshape((3, 4)), index=sids, columns=types)
+
+            for i in files:
+                open(join(d, i), 'w').close()
+            obs = create_sample_table(
+                d,
+                sid=lambda x: re.split('_R[12]_', x)[0],
+                select=lambda x: x.endswith('.fq'))
+
+            self.assertEqual(exp.to_dict(), obs.to_dict())
+
 
 if __name__ == '__main__':
     unittest.main()
