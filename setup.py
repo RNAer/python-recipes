54b7541f561ed40d921d6cff9c19f9128e0efe45
Zech Xu <zhenjiang.xu@gmail.com>
2016-06-11 17:08:17 -0700

    add dep in setup.py

--- setup.py
+++ setup.py
@@ -9,4 +9,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
     ],
-)
+    install_requires=['numpy', 'pandas'],
+    extras_require={'test': ["nose", "pep8", "flake8"],
+                    'coverage': ["coverage"],
+                    'doc': ["Sphinx"]})
