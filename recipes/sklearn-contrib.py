c38bec6a7dc84a72d5f5d6c1269a788ecd293dfc
Zech Xu <zhenjiang.xu@gmail.com>
2018-05-03 13:49:11 -0700

    add weighted scoring function

--- recipes/sklearn-contrib.py
+++ recipes/sklearn-contrib.py
@@ -1,4 +1,40 @@
+import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.metrics import make_scorer
+
+
+def weighted_score(yobs, yhat, weights):
+    '''Compute the accuracy score with weights.
+
+    Parameters
+    ----------
+    yobs : array-like int or str
+        true labels
+    yhat : array-like int or str
+        predicted labels
+    weights : numeric
+        the weights for each prediction
+
+    Returns
+    -------
+    float
+        the weighted accuracy score (between 0 and 1)
+
+    Examples
+    --------
+    >>> yobs = ['y', 'y', 'n', 'n', 'n']
+    >>> yhat = ['y', 'n', 'n', 'y', 'n']
+    >>> weights = [2, 1, 2, 3, 2]
+    >>> score = weighted_score(yobs, yhat, weights)
+    >>> score == 0.6
+    True
+    '''
+    agrees = np.array(yobs) == np.array(yhat)
+    score = np.sum(agrees.astype(int) * np.array(weights))
+    return score / np.sum(weights)
+
+
+weighted_scorer = make_scorer(weighted_score)
 
 
 # Inspired from stackoverflow.com/questions/25239958
